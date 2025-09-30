9f180e5f3c9acfd541ef013e6c8e1cef34ac4e2c
Akari <bakari82@mail.ru>
2025-09-30 19:01:56 +0500

    Add tim from Noah.

---
+++ src/psyq/tim.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct TIM_IMAGE
+{
+    u32 mode;
+    struct RECT* crect;
+    const void* caddr;
+    struct RECT* prect;
+    const void* paddr;
+};
+
+int OpenTIM( std::vector<u8>::const_iterator ptr );
+TIM_IMAGE* ReadTIM( TIM_IMAGE* timimg );
+
+void StoreImage( struct RECT* rect, std::vector<u16>& output );
+void LoadImage( struct RECT* pRect, const u8* data );
+void LoadImage( struct RECT* pRect, std::span<u8>::iterator data );
+void LoadImage( struct RECT* pRect, std::span<u16>::iterator data );
+void loadTimToVram( std::vector<u8>::const_iterator ptr, short imageX, short imageY, short palX, short palY, short palW, short palH );
